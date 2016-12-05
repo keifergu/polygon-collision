9bd8250c1d28c734577e584bbc712866c7784dd1
Keifer.Gu <keifergu@gmail.com>
2016-12-05 19:15:01 +0800

    添加collision测试文件；修复错误：

---
+++ test/collision.test.js
@@ -0,0 +1,47 @@
+let rewire = require('rewire');
+let should = require('chai').should;
+let collision = require('../lib/collision');
+// 此处使用 rewire ，便于测试内部没有暴露出来的属性
+let rewireCollision = rewire('../lib/collision.js');
+
+should();
+
+let cget = rewireCollision.__get__;
+
+describe('Collision', () => {
+	let polygonPoints = [
+	        [[250, 130], [250, 250],
+	         [350, 250], [400, 150]],
+	        [[400, 130], [400, 200],
+	         [490, 100], [450, 10]]
+	    ];
+  const shapeWord = {
+  	line: "line",
+  	point: "point",
+  	circle: "circle",
+  	polygon: "polygon",
+  };
+
+	describe('#polygon with circle', () => {
+		it('collision ', () => {
+			let dp1 = {
+					type: shapeWord.polygon,
+					points: polygonPoints[0].map((v) => {return {x:v[0],y:v[1]};}),
+				},
+				dc1 = {
+					type: shapeWord.circle,
+					points: [{x:50,y:40}],
+					r: 40,
+				},
+				dc2 = {
+					type: shapeWord.circle,
+					points: [{x:250, y:240}],
+					r: 20,
+				};
+			let res1 = collision(dp1, dc1);
+			let res2 = collision(dp1, dc2);
+			res2.should.to.be.equal(true);
+			res1.should.to.be.equal(false);
+		});
+	});
+});
